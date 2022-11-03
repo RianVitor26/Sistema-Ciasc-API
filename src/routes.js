2683abdd9709058d224f305020f413dddf2c38c6
RianVitor26 <rianvitorlhe@gmail.com>
2022-11-02 21:00:08 -0300

    criando rota home

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,10 @@ import HelloController from './controllers/HelloController'
 
 const routes = new Router()
 
+routes.get('/', (req, res) => {
+    res.send("Home")
+})
+
 routes.get('/hello', HelloController.index)
 
 export default routes
